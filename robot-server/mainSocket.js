a75956a1ac649a35c7c97917cdc1f967578d068f
jljorgenson18 <jljorgenson18@gmail.com>
2014-12-05 19:28:51 -0600

    Fixed module scoping issue... Fucking node.js. Also threw up an h2 so we know its connected

--- robot-server/mainSocket.js
+++ robot-server/mainSocket.js
@@ -1,15 +1,3 @@
 // Link socket.io to the previously created server
-var io = require('socket.io').listen(server);
 
 // When someone has connected to me...
-io.sockets.on('connection', function (socket) {
-    // Send out a message (only to the one who connected)
-    socket.emit('robot connected', {
-        data: 'Connected'
-    });
-
-    // When I've received 'robot command' message from this connection...
-    socket.on('robot command', function (data) {
-        console.log(data);
-    });
-});
--- server.js
+++ server.js
@@ -3,16 +3,29 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var path = require('path');
-var mainSocket = require('robot-server/mainSocket.js');
 
 // Access server through port 80
 server.listen(8080);
 
+var io = require('socket.io').listen(server);
+
 // Set '/public' as the static folder. Any files there will be directly sent to the viewer
 app.use(express.static(__dirname + '/robot-ui'));
-
+console.log("Now listening on port 8080...");
 // Set index.html as the base file
 app.get('/', function (req, res) {
     console.log("Sending html...");
     res.sendFile(path.resolve(__dirname + '../robot-ui/index.html'));
+});
+
+io.sockets.on('connection', function (socket) {
+    // Send out a message (only to the one who connected)
+    socket.emit('robot connected', {
+        data: 'Connected'
+    });
+
+    // When I've received 'robot command' message from this connection...
+    socket.on('robot command', function (data) {
+        console.log(data);
+    });
 });
