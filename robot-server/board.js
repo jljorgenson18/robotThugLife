622bda37c76809ad48cb69ada91f0ed964c1ccc6
jljorgenson18 <jljorgenson18@gmail.com>
2014-12-10 22:49:51 -0600

    Threw in some log commands and tried to fix it

--- robot-server/board.js
+++ robot-server/board.js
@@ -1,30 +1,38 @@
 module.exports.init = function () {
+    console.log("On Init");
     var j5 = require("johnny-five"),
-        board, ledOne, ledTwo;
+        board, self;
 
     // collect command information from server. left toggles led 1 right toggles led 2
 
     // Initialize connection to Arduino (will crash if none is attached)
     board = new j5.Board();
-
     // When the connection is ready...
+    self = board;
+    console.log("SELF!");
+    console.log(self);
     board.on("ready", function () {
         // create LEDs
-        ledOne = new j5.Led(13)
-        ledTwo = new j5.Led(11)
+        console.log("This =");
+        console.log(this);
+        self.ledOne = new j5.Led(13)
+        self.ledTwo = new j5.Led(11)
 
         // Inject LEDs
         board.repl.inject({
-            led1: ledOne
+            led1: self.ledOne
         });
         board.repl.inject({
-            led2: ledTwo
-        })
+            led2: self.ledTwo
+        });
+        console.log(board);
     });
     return board;
 }
 
 module.exports.sendToBot = function (board, data) {
+    var ledOne = board.ledOne;
+    var ledTwo = board.ledOne;
     var command = data.command;
     if (command == 'led1-On') {
         ledOne.on();
--- robot-server/mainSocket.js
+++ robot-server/mainSocket.js
@@ -1,6 +1,6 @@
 module.exports.init = function (server, board) {
     var io = require('socket.io').listen(server);
-
+    var connectToJ5 = true;
     io.sockets.on('connection', function (socket) {
         // Send out a message (only to the one who connected)
         socket.emit('robot connected', {
@@ -11,7 +11,15 @@ module.exports.init = function (server, board) {
         socket.on('robot command', function (data) {
             console.log(data);
             if (data.command === 'connected') {
+                connectToJ5 = true;
+            }
+            if (data.command === "debugConnected") {
+                connectToJ5 = false;
+            }
+            if (connectToJ5) {
                 var myBoard = board.init();
+                console.log(board);
+                console.log(myBoard);
                 board.sendToBot(myBoard, data);
             }
         });
