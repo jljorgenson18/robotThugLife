656e004f5029beb00afabd164fa93f6db9987eae
John <midnighttripmusc@gmail.com>
2014-12-10 20:02:26 -0500

    fucking data nonsense bullshit

--- server.js
+++ server.js
@@ -31,3 +31,53 @@ io.sockets.on('connection', function (socket) {
         console.log(data);
     });
 });
+
+
+//// JOHNNY FIVE SHIT
+
+var five = require("johnny-five")
+    , board, ledOne, ledTwo;
+
+
+
+
+// Initialize connection to Arduino (will crash if none is attached)
+board = new five.Board();
+
+// When the connection is ready...
+board.on("ready", function() {
+  
+  // create LEDs
+  ledOne = new j5.Led(13)
+  ledTwo = new j5.Led(11)
+  
+
+  // Inject LEDs
+  board.repl.inject({
+    led1: ledOne
+  });
+  board.repl.inject({
+  led2: ledTwo
+  })
+});
+
+// collect command information from server. left toggles led 1 right toggles led 2
+
+var left = false;
+var right = false;
+
+var command = data.command;
+if (command == 'led1-On') {
+  ledOne.on();
+
+  board.repl.inject({
+    led1: ledOne
+  });
+}
+else if (command == 'led1-Off') {
+  led.off()
+
+  board.repl.inject({
+    led1: ledOne
+  });
+}
