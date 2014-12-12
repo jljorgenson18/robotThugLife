b1d2db24ee8f81d6b340eb9d3c20575bd1752c81
jljorgenson18 <jljorgenson18@gmail.com>
2014-12-12 09:05:04 -0600

    Sent back json object on initial connection

--- robot-server/mainSocket.js
+++ robot-server/mainSocket.js
@@ -3,10 +3,14 @@ module.exports.init = function (server, board) {
     var connectToJ5 = true;
     io.sockets.on('connection', function (socket) {
         // Send out a message (only to the one who connected)
-        socket.emit('robot connected', {
-            data: 'Connected'
-        });
         var myBoard = board("firstBot");
+        var uiConfig = require('./firstBot/uiConfig.json');
+        // var uiConfig = JSON.parse(uiConfigJSON.readFileSync('file', 'utf8'));
+        socket.emit('robot connected', {
+            data: 'Connected',
+            bot: uiConfig
+        });
+
         // When I've received 'robot command' message from this connection...
         socket.on('robot command', function (data) {
             console.log(data);
