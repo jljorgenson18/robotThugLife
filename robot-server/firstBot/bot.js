391d815c8470c5f50cbb0afcdd926f1e3a742b55
jljorgenson18 <jljorgenson18@gmail.com>
2015-03-03 23:51:40 -0600

    Split out the ledFlash function. Tweaked delays

--- robot-server/firstBot/bot.js
+++ robot-server/firstBot/bot.js
@@ -1,11 +1,11 @@
-module.exports = function () {
+module.exports = function() {
     console.log('Constructing Bot...');
     var bot = {};
     var j5 = require("johnny-five");
 
     bot.board = new j5.Board();
     bot.ready = false;
-    bot.board.on("ready", function () {
+    bot.board.on("ready", function() {
         console.log("ready freddy");
         initComponents();
         initListeners();
@@ -14,43 +14,50 @@ module.exports = function () {
     return bot;
 
     function initComponents() {
-      var ledPins = [2,3,4,5,6,7,8,9];
-      bot.leds = [];
+        var ledPins = [2, 3, 4, 5, 6, 7, 8, 9];
+        bot.leds = [];
 
-      for (var i = 0; i < ledPins.length; i++){
-        var myLed = new j5.Led(ledPins[i]);
-        bot.leds.push(myLed);
-      }
+        for(var i = 0; i < ledPins.length; i++) {
+            var myLed = new j5.Led(ledPins[i]);
+            bot.leds.push(myLed);
+        }
     }
 
     function initListeners() {
-        bot.board.on('util1', function () {
-          for (var i = 0; i < bot.leds.length; i++) {
-            bot.leds[i].on();
-          }
+        bot.board.on('util1', function() {
+            for(var i = 0; i < bot.leds.length; i++) {
+                bot.leds[i].on();
+            }
         });
 
-        bot.board.on('util2', function () {
-          for (var i = 0; i < bot.leds.length; i++) {
-            bot.leds[i].off();
-          }
+        bot.board.on('util2', function() {
+            for(var i = 0; i < bot.leds.length; i++) {
+                bot.leds[i].off();
+            }
         });
-        bot.board.on('util3', function () {
-          var delay = 1;
-          bot.board.counter = 0;
-          for (var i = 0; i < bot.leds.length; i++) {
-            var led = bot.leds[i];
-            bot.board.wait(delay,function(){
-                console.log(this.counter + " on");
-                bot.leds[this.counter].on();
-            });
-            bot.board.wait(delay + 200,function(){
-                console.log(this.counter + " off");
-                bot.leds[this.counter].off();
-                this.counter = (this.counter + 1) % bot.leds.length;
-            });
-            delay += 500;
-          }
+        bot.board.on('util3', function() {
+            var delay = 0;
+            //var loopLength = bot.leds.length;
+            var loopLength = 2000;
+            bot.board.counter = 0;
+            for(var i = 0; i < loopLength; i++) {
+                ledFlash(i, delay);
+                delay += 200;
+            }
+
+        });
+    }
+
+    function ledFlash(idx, delay) {
+        var led = bot.leds[idx];
+        bot.board.wait(delay, function() {
+            //console.log(bot.board.counter + " on");
+            bot.leds[bot.board.counter].on();
+        });
+        bot.board.wait(delay + 500, function() {
+            //console.log(bot.board.counter + " off");
+            bot.leds[bot.board.counter].off();
+            bot.board.counter = (bot.board.counter + 1) % bot.leds.length;
         });
     }
 };
